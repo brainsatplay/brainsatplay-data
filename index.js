ae868699812376da43bfa6336c7d0de72c00d835
Garrett <gflynn@usc.edu>
2022-02-21 20:34:40 -0800

    Updated for bundling

--- index.js
+++
@@ -1,6 +0,0 @@
-export * as DS from './DataStructures.js'
-export * as DM from './DataManipulation.js'
-export * from './DataTablet.js'
-export * from './DataThread.js'
-export * from './DataConversion.js'
-
---
+++ src/index.js
@@ -0,0 +1,11 @@
+import * as DS from './DataStructures.js'
+import * as DM from './DataManipulation.js'
+export * from './DataTablet.js'
+export * from './DataThread.js'
+export * from './DataConversion.js'
+
+export {
+    DS,
+    DM
+}
+
